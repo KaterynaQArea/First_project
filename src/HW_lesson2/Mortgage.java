cc320a9bf6a25ad909a6ea7c63c15e816e4ec363
samilyk <qareatestqarea@gmail.com>
2021-07-17 00:28:33 +0300

    camel case in ClassName

--- src/HW_lesson2/Mortgage.java
+++ src/HW_lesson2/Mortgage.java
@@ -2,21 +2,13 @@ package HW_lesson2;
 
 public class Mortgage {
     public static void main(String[] args) {
-        int husbandAge = 34;
-        int wifeAge = 34;
+        int husbandAge = 27;
+        int wifeAge = 28;
         boolean b = methodAge(husbandAge, wifeAge);
         System.out.println(b);
-        if (b == true)
-        System.out.println( "They’re both under 35 years old");
-        else
-            System.out.println("Some of them over 35");
     }
     public static boolean methodAge(int husbandAge, int wifeAge){
-        if (husbandAge < 35 && wifeAge < 35 )
-            return true;
-        else
-            return false;
-
+        return (husbandAge < 35 && wifeAge < 35 );
     }
 }
 
--- src/HW_lesson2/Purchase.java
+++ src/HW_lesson2/Purchase.java
@@ -2,14 +2,14 @@ package HW_lesson2;
 
 public class Purchase {
     public static void main(String[] args) {
-    int PresenceOfClass_1_apples = 0;
-    int PresenceOfClass_2_apples = 23;
-    boolean presence = methodPurchase (PresenceOfClass_1_apples,PresenceOfClass_2_apples);
+    int presenceOfClass_1_apples = 0;
+    int presenceOfClass_2_apples = 23;
+    boolean presence = methodPurchase (presenceOfClass_1_apples,presenceOfClass_2_apples);
         System.out.println(presence);
     }
 
-    public static boolean methodPurchase(int PresenceOfClass_1_apples, int PresenceOfClass_2_apples) {
-        return PresenceOfClass_1_apples > 0 || PresenceOfClass_2_apples > 0;
+    public static boolean methodPurchase(int presenceOfClass_1_apples, int presenceOfClass_2_apples) {
+        return presenceOfClass_1_apples > 0 || presenceOfClass_2_apples > 0;
         /*if (PresenceOfClass_1_apples > 0 || PresenceOfClass_2_apples >0)
                 return true;
         else
