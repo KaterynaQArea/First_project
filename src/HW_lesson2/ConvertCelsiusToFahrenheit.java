66d5c1cbeacae1fe051d37ff51b0d26355b945b4
samilyk <qareatestqarea@gmail.com>
2021-07-17 00:47:41 +0300

    Data types, operators, variables

---
+++ src/HW_lesson2/ConvertCelsiusToFahrenheit.java
@@ -0,0 +1,10 @@
+package HW_lesson2;
+
+public class ConvertCelsiusToFahrenheit {
+    public   static   void   main(String[]   args)   {
+        double   celsiusTemperature   =   50;
+        double   fahrenheitTemperature   =   (celsiusTemperature * 9 / 5d ) + 32;
+        System. out .println("For  celsius  temperature   "   +   celsiusTemperature
+                +   " fahrenheit  temperature   is:   "   +   fahrenheitTemperature);
+    }
+}
---
+++ src/HW_lesson2/ConvertFahrenheitToCelsius.java
@@ -0,0 +1,10 @@
+package HW_lesson2;
+
+public class ConvertFahrenheitToCelsius {
+    public   static   void   main(String[]   args)   {
+        double   fahrenheitTemperature   =   77;
+        double   celsiusTemperature   =   5 / 9d * (fahrenheitTemperature - 32);
+        System. out .println("For   fahrenheit   temperature   "   +   fahrenheitTemperature
+                +   "   celsius   temperature   is:   "   +   celsiusTemperature);
+    }
+}
