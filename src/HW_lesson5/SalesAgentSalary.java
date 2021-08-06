ff9c39042f48c2d538dd05ea1166335a4d371e1f
samilyk <qareatestqarea@gmail.com>
2021-08-06 13:06:29 +0300

    Methods

--- src/HW_lesson5/SalesAgentSalary.java
+++ src/HW_lesson5/SalesAgentSalary.java
@@ -6,47 +6,48 @@ public class SalesAgentSalary {
     public static void main(String[] args) {
         Salary salary = new Salary();
         int hours = 176;
-        double bonusHours = salary.overWorkBonus(hours, 21.5);
-        System.out.println("Overworking bonus is equal: " + bonusHours + "$");
-        double rate = salary.rateForBaseHours(7);
-        System.out.println("Coefficient for base hours is equal: " + rate + " $/h");
-        double bonusForSale = salary.bonusForSale(21);
-        System.out.println("Bonus for sales is equal: " + bonusForSale + "$");
-        double salesBonus = salary.salesBonus(36578.50);
-        System.out.println("Bonus for sum of sales is equal: " + salesBonus + "$");
-        double finalSalary = salary.finalSalary(hours, rate, bonusHours, bonusForSale, salesBonus);
+        double rate = 11;
+        double overtimeBonus = salary.overtimeBonus(hours, rate);
+        System.out.println("Overworking bonus is equal: " + overtimeBonus + "$");
+        double coefficient = salary.coefficientForBaseHours(7);
+        System.out.println("Coefficient for base hours is equal: " + coefficient);
+        double bonusForNumberOfSales = salary.bonusForNumberOfSales(25);
+        System.out.println("Bonus for sales is equal: " + bonusForNumberOfSales + "$");
+        double bonusForSumOfSales = salary.bonusForSumOfSales(36578.50);
+        System.out.println("Bonus for sum of sales is equal: " + bonusForSumOfSales + "$");
+        double finalSalary = salary.finalSalary(hours, rate, coefficient, overtimeBonus, bonusForNumberOfSales, bonusForSumOfSales);
         System.out.println("Salary is equal: " + finalSalary + "$");
     }
 }
 
 class Salary {
-    double rate;
+    double coefficient;
     double bonus;
 
-    double overWorkBonus(int hours, double rate) {
+    double overtimeBonus(int hours, double rate) {
         if (hours > 160) {
-            int overhours = hours - 160;
-            double bonus = overhours * rate * 1.5;
+            int overtime = hours - 160;
+            double bonus = overtime * rate * 1.5;
             return bonus;
         } else {
             return 0;
         }
     }
 
-    double rateForBaseHours(int years) {
+    double coefficientForBaseHours(int years) {
         if (years < 2) {
-            rate = 1;
+            coefficient = 1;
         } else if (years >= 2 && years < 4) {
-            rate = 1.2;
+            coefficient = 1.2;
         } else if (years >= 4 && years < 6) {
-            rate = 1.3;
+            coefficient = 1.3;
         } else if (years >= 6) {
-            rate = 1.4;
+            coefficient = 1.4;
         }
-        return rate;
+        return coefficient;
     }
 
-    double bonusForSale(int num) {
+    double bonusForNumberOfSales(int num) {
         if (num >= 20) {
             bonus = 250;
         } else if (num < 10) {
@@ -55,15 +56,15 @@ class Salary {
         return bonus;
     }
 
-    double salesBonus(double sum) {
+    double bonusForSumOfSales(double sum) {
         if (sum > 15000) {
             bonus = 500;
         }
         return bonus;
     }
 
-    double finalSalary(int hours, double rate, double bonusHours, double bonusForSale, double salesBonus) {
-        double salary = hours * rate + bonusHours + bonusForSale + salesBonus;
+    double finalSalary(int hours, double rate, double coefficient, double overtimeBonus, double bonusForNumberOfSales, double bonusForSumOfSales) {
+        double salary = 160 * rate * coefficient + overtimeBonus + bonusForNumberOfSales + bonusForSumOfSales;
         return salary;
     }
 }
